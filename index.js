4a4ee997c5b075e5628b50432d28069c6bff2041
eyal-hl <eyalhermonilahat@gmail.com>
2022-02-16 20:53:13 +0200

    test from heroku servers

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 if (process.env.NODE_ENV == 'development'){ require('dotenv').config(); }
 
 const { Telegraf } = require('telegraf')
+const PORT = process.env.PORT || 3000;
+const URL = process.env.URL || 'https://eyals-telegram-bot.herokuapp.com';
+
+  
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
+bot.use(async (ctx, next) => {
+    console.log(ctx);
+    next();
+})
+
 bot.start((ctx) => ctx.reply('Welcome'))
 bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.on('sticker', (ctx) => ctx.reply('👍'))
 bot.hears('hi', (ctx) => ctx.reply('Hey there'))
-bot.launch()
 
-// Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'))
-process.once('SIGTERM', () => bot.stop('SIGTERM'))
+
+bot.telegram.setWebhook(`${URL}/bot${process.env.BOT_TOKEN}`);
+bot.startWebhook(`/bot${process.env.BOT_TOKEN}`, null, PORT)
