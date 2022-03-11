a9b98b2df7d2a02aa8b08a8f0b25d6274f392d8f
eyal-hl <eyalhermonilahat@gmail.com>
2022-03-12 00:32:27 +0200

    added compiled

---
+++ dist/index.js
@@ -0,0 +1,36 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+if (process.env.HEROKU == undefined) {
+    require('dotenv').config();
+}
+const { Telegraf } = require('telegraf');
+const PORT = process.env;
+const HEROKU_URL = process.env.HEROKU_URL;
+const bot = new Telegraf(process.env.BOT_TOKEN);
+bot.use((ctx, next) => __awaiter(void 0, void 0, void 0, function* () {
+    console.log(ctx);
+    next();
+}));
+bot.start((ctx) => ctx.reply('Welcome'));
+bot.help((ctx) => ctx.reply('Send me a sticker'));
+bot.on('sticker', (ctx) => ctx.reply('👍'));
+bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+if (process.env.HEROKU == undefined) {
+    bot.launch();
+}
+else {
+    bot.telegram.setWebhook(`${URL}/bot${process.env.BOT_TOKEN}`);
+    bot.startWebhook(`/bot${process.env.BOT_TOKEN}`, null, PORT);
+}
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
+//# sourceMappingURL=index.js.map
