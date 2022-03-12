16a2b4de4eaed462cbf9abb58ca3a1c1f3fbe991
eyal-hl <eyalhermonilahat@gmail.com>
2022-03-12 11:12:50 +0200

    .

--- src/index.ts
+++ src/index.ts
@@ -11,20 +11,19 @@ const PORT = process.env;
 const HEROKU_URL = process.env.HEROKU_URL;
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
-console.log("1");
+
 bot.use(async (ctx:Context, next:()=>Promise<void>) => {
     console.log(ctx);
     next();
 })
-console.log("2");
 bot.start((ctx:Context) => ctx.reply('Welcome'))
 bot.help((ctx:Context) => ctx.reply('Send me a sticker'))
 bot.on('sticker', (ctx:Context) => ctx.reply('👍'))
 bot.hears('hi', (ctx:Context) => ctx.reply('Hey there'))
-console.log("3");
 if (process.env.HEROKU == undefined){
     bot.launch()
 }else{
+    console.log("2");
     bot.telegram.setWebhook(`${HEROKU_URL}/bot${process.env.BOT_TOKEN}`);
     bot.startWebhook(`/bot${process.env.BOT_TOKEN}`, null, PORT)
 }
