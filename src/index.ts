d829f7f02f3bc21951b927534fe725d48a6b38d6
eyal-hl <eyalhermonilahat@gmail.com>
2022-03-12 11:31:17 +0200

    clearer

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,28 @@
-import { Context, NarrowedContext } from "telegraf";
+import { Context } from "telegraf";
 
 if (process.env.NODE_ENV != 'production'){ require('dotenv').config(); }
-console.log("1");
+
 const { Telegraf } = require('telegraf')
 const PORT = process.env.PORT;
 const HEROKU_URL = process.env.HEROKU_URL;
-
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
 bot.use(async (ctx:Context, next:()=>Promise<void>) => {
     console.log(ctx);
     next();
 })
+
 bot.start((ctx:Context) => ctx.reply('Welcome'))
 bot.help((ctx:Context) => ctx.reply('Send me a sticker'))
 bot.on('sticker', (ctx:Context) => ctx.reply('👍'))
 bot.hears('hi', (ctx:Context) => ctx.reply('Hey there'))
+
+
 if (process.env.NODE_ENV === 'production') {
     bot.launch({
         webhook: {
             domain: HEROKU_URL,
-            port: parseInt(process.env.PORT || '3000')
+            port: PORT
         }
     })
 } else {
