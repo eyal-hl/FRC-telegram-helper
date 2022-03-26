f277954bfff6b2b364cfe9397159c25ffa05390a
eyal-hl <eyalhermonilahat@gmail.com>
2022-03-26 22:16:43 +0300

    splitting long messages

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,28 @@
+if (process.env.NODE_ENV != 'production'){ require('dotenv').config(); }
 import { json } from "stream/consumers";
 import { Context, Telegraf } from "telegraf";
 import { router } from "./router";
 
-if (process.env.NODE_ENV != 'production'){ require('dotenv').config(); }
+const maxMessageSize = 4096;
+const splitMessage = (message:string):string[] => {
+    let lines = message.split('\n');
+    let reply:string[] = [];
+    let currPart = "";
+
+    lines.forEach(line => {
+        if (currPart.length + line.length + 1 >= maxMessageSize){
+            reply.push(currPart);
+            currPart = "";
+        }
+        currPart += line + '\n';
+    })
+
+    reply.push(currPart)
+
+    return reply
+}
+
+
 const PORT = process.env.PORT || "3000";
 const HEROKU_URL = process.env.HEROKU_URL;
 const bot = new Telegraf(process.env.BOT_TOKEN||"")
@@ -15,8 +35,8 @@ bot.start((ctx:Context) => ctx.reply('Welcome'))
 bot.help((ctx:Context) => ctx.reply('Send me a sticker'))
 bot.on('sticker', (ctx:Context) => ctx.reply('👍'))
 bot.hears('hi', (ctx:Context) => ctx.reply('Hey there'))
-bot.on('text', (ctx)=>{
-    ctx.reply(router(ctx.message.text));
+bot.on('text', async (ctx)=>{
+    splitMessage(await router(ctx.message.text)).forEach(async line => await ctx.reply(line))
 })
 
 
